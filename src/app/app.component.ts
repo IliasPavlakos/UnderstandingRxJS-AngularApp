23fa1a098645535d1490e8bbaef1ad27b1f823fd
ipavlakos <pavlakosIlias@outlook.com>
2024-08-17 18:49:00 +0300

    Creating and using a custom Observable from scratch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
-import {interval} from "rxjs";
+import {interval, Observable} from "rxjs";
 import {toObservable, toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
@@ -14,6 +14,20 @@ export class AppComponent implements OnInit {
   intervalSignal = toSignal(this.interval$, {initialValue: 0});
   // interval = signal(0);q
   // doubleInterval = computed(() => this.interval() * 2);
+  customInterval$ = new Observable((subscriber) => {
+    // subscriber.error('data');
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      if(timesExecuted > 3){
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...')
+      subscriber.next({message: 'New value'});
+      timesExecuted++;
+    }, 1000);
+  })
   private destroyRef = inject(DestroyRef);
 
   constructor() {
@@ -37,8 +51,16 @@ export class AppComponent implements OnInit {
     //   subscription.unsubscribe();
     // })
 
-    const subscription = this.clickCount$.subscribe({
-      next: value => console.log(`Clicked button ${this.clickCount()} times`)
+    // const subscription = this.clickCount$.subscribe({
+    //   next: value => console.log(`Clicked button ${this.clickCount()} times`)
+    // })
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // })
+
+    const subscription = this.customInterval$.subscribe({
+      next: value => console.log(value),
+      complete: () => console.log('Completed'),
     })
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
